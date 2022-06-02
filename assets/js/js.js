9b7d4dd7d49952205df18f5049b9a1241672c9ca
Gustavo Andrade <gustavoandradedeveloper@gmail.com>
2022-06-02 14:22:59 -0300

    Fazendo umas configuração no layout

---
+++ assets/js/js.js
@@ -0,0 +1,32 @@
+function consultar(cep){	
+
+    let url = 'https://viacep.com.br/ws/'+cep+'/json/';
+
+    let objXmlHttpRequest = new XMLHttpRequest;
+
+    objXmlHttpRequest.open('GET', url);
+
+    //console.log(objXmlHttpRequest);
+
+    objXmlHttpRequest.onreadystatechange = () => {
+        
+        if(objXmlHttpRequest.readyState == 4 && objXmlHttpRequest.status == 200){
+            let dadosText = objXmlHttpRequest.responseText;
+            let dadosCepObj = JSON.parse(dadosText);
+            //console.log(dadosCepObj);
+            
+            let endereco = dadosCepObj.logradouro;
+            let Bairro = dadosCepObj.bairro;
+            let Cidade = dadosCepObj.localidade;
+            let uf = dadosCepObj.uf;
+
+            document.getElementById('endereco').value = endereco ;
+            document.getElementById('bairro').value = Bairro ;
+            document.getElementById('cidade').value = Cidade ;
+            document.getElementById('uf').value = uf ;
+    } 
+}
+
+objXmlHttpRequest.send();
+
+}
